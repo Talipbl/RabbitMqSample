0a17c28c35bdcdea3b7b0ed36f31a65118bad89e
Talip BULUNDU <talipbulundu@gmail.com>
2022-08-01 17:37:11 +0300

    Add project files.

---
+++ RabbitMqConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("ikincikuyruk", true, false, true);
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume("ikincikuyruk", false, consumer);
+    consumer.Received += (sender, args) =>
+    {
+        var body = args.Body.Span;
+        Console.WriteLine(Encoding.UTF8.GetString(body));
+    };
+    Console.ReadLine();
+}
---
+++ RabbitMqSample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("ikincikuyruk",true,false,true);
+    byte[] byteMessage = Encoding.UTF8.GetBytes("Bu bir RabbitMQ kuyruğunun ikinci mesajıdır!");
+    channel.BasicPublish(exchange: "", routingKey: "ikincikuyruk", body: byteMessage);
+}
